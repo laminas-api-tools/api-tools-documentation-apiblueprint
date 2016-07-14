e28b3732d70d9930dee4f7a153bb75beb5ff7b00
Matthew Weier O'Phinney <matthew@zend.com>
2016-07-14 11:47:46 -0500

    Updated factory to be forwards compatible with zend-servicemanager v3.

--- src/ApiBlueprintViewStrategyFactory.php
+++ src/ApiBlueprintViewStrategyFactory.php
@@ -1,20 +1,22 @@
 <?php
 /**
  * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
- * @copyright Copyright (c) 2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2015-2016 Zend Technologies USA Inc. (http://www.zend.com)
  * @copyright Copyright (c) 2015 Apiary Ltd. <support@apiary.io>
  */
 
 namespace ZF\Apigility\Documentation\ApiBlueprint;
 
+use Interop\Container\ContainerInterface;
+
 class ApiBlueprintViewStrategyFactory
 {
     /**
-     * @param \Zend\ServiceManager\ServiceLocatorInterface $services
+     * @param ContainerInterface $container
      * @return ApiBlueprintViewStrategy
      */
-    public function __invoke($services)
+    public function __invoke(ContainerInterface $container)
     {
-        return new ApiBlueprintViewStrategy($services->get(__NAMESPACE__ . '\ApiBlueprintViewRenderer'));
+        return new ApiBlueprintViewStrategy($container->get(ApiBlueprintViewRenderer::class));
     }
 }
