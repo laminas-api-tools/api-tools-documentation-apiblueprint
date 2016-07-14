683ad2a7739e8f33e29c4d381d55acfd240db478
Matthew Weier O'Phinney <matthew@zend.com>
2016-07-14 11:50:09 -0500

    Added method docblock

--- src/Api.php
+++ src/Api.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
- * @copyright Copyright (c) 2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2015-2016 Zend Technologies USA Inc. (http://www.zend.com)
  * @copyright Copyright (c) 2015 Apiary Inc. <support@apiary.io>
  */
 
@@ -46,6 +46,11 @@ class Api
         return $this->resourceGroups;
     }
 
+    /**
+     * Iterate API services and create internal resource groups.
+     *
+     * @return void
+     */
     private function createResourceGroups()
     {
         foreach ($this->api->getServices() as $service) {
