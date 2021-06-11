707313fe2679478e418762c8e87429e36623bd6c
Gary Lockett <gary@creativecow.uk>
2021-06-11 11:12:54 +0100

    fix(cs): ensure coding standards pass
    Signed-off-by: Gary Lockett <gary@creativecow.uk>

--- test/ResourceTest.php
+++ test/ResourceTest.php
@@ -1,9 +1,11 @@
 <?php
 
 /**
+ * @codingStandardsIgnoreStart Generic.Files.LineLength.TooLong
  * @see       https://github.com/laminas-api-tools/api-tools-documentation-apiblueprint for the canonical source repository
  * @copyright https://github.com/laminas-api-tools/api-tools-documentation-apiblueprint/blob/master/COPYRIGHT.md
  * @license   https://github.com/laminas-api-tools/api-tools-documentation-apiblueprint/blob/master/LICENSE.md New BSD License
+ * @codingStandardsIgnoreEnd Generic.Files.LineLength.TooLong
  */
 
 namespace LaminasTest\ApiTools\Documentation\ApiBlueprint;
